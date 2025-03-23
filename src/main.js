f596b07e299ebd59acd7b7a9420c6c67019b34b4
Prajun Trital <prajuncs@gmail.com>
2025-03-23 18:55:38 -0500

    small player

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { Scene } from "./modules/scene.js";
 import { Environment } from "./modules/environment.js";
+import { Player } from "./modules/player.js";
 
 class Game {
   constructor() {
     this.scene = new Scene();
     this.environment = new Environment(this.scene);
+    this.player = new Player(this.scene);
 
     // Start animation loop
     this.animate = this.animate.bind(this);
@@ -13,6 +15,7 @@ class Game {
 
   animate() {
     requestAnimationFrame(this.animate);
+    this.player.update();
     this.scene.render();
   }
 }
--- src/modules/player.js
+++ src/modules/player.js
@@ -1,98 +1,47 @@
 export class Player {
-  constructor(scene, physics) {
+  constructor(scene) {
     this.scene = scene;
-    this.physics = physics;
 
     // Player settings
-    this.speed = 5;
-    this.jumpForce = 5;
-    this.lateralSpeed = 3;
-    this.isJumping = false;
+    this.speed = 0.3;
+    this.lateralSpeed = 0.2;
 
-    // Start position
-    this.startPosition = { x: 0, y: 3, z: 0 };
     this.createPlayer();
     this.setupControls();
   }
 
   createPlayer() {
-    // Visual representation
+    // Create a simple player cube
     const geometry = new THREE.BoxGeometry(1, 2, 1);
     const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
     this.mesh = new THREE.Mesh(geometry, material);
-    this.mesh.position.copy(this.startPosition);
+
+    // Start position
+    this.mesh.position.set(0, 1, 0);
     this.scene.scene.add(this.mesh);
-
-    // Physics body
-    this.body = this.physics.createBox(
-      { x: 1, y: 2, z: 1 },
-      this.startPosition,
-      5 // mass
-    );
-
-    // Set initial velocity
-    this.body.velocity.set(0, 0, -this.speed);
-    this.physics.addBody(this.body);
   }
 
   setupControls() {
     document.addEventListener("keydown", (e) => {
       switch (e.key) {
         case "ArrowLeft":
-          this.moveLeft();
+          if (this.mesh.position.x > -6) {
+            // Prevent going off runway
+            this.mesh.position.x -= this.lateralSpeed;
+          }
           break;
         case "ArrowRight":
-          this.moveRight();
-          break;
-        case " ": // Space
-          this.jump();
-          break;
-        case "Enter":
-          this.sprint();
+          if (this.mesh.position.x < 6) {
+            // Prevent going off runway
+            this.mesh.position.x += this.lateralSpeed;
+          }
           break;
       }
     });
   }
 
-  moveLeft() {
-    if (this.body.velocity.x > -this.lateralSpeed) {
-      this.body.velocity.x = -this.lateralSpeed;
-    }
-  }
-
-  moveRight() {
-    if (this.body.velocity.x < this.lateralSpeed) {
-      this.body.velocity.x = this.lateralSpeed;
-    }
-  }
-
-  jump() {
-    if (!this.isJumping) {
-      this.body.velocity.y = this.jumpForce;
-      this.isJumping = true;
-      setTimeout(() => {
-        this.isJumping = false;
-      }, 1000);
-    }
-  }
-
-  sprint() {
-    this.speed *= 1.5;
-    setTimeout(() => {
-      this.speed /= 1.5;
-    }, 1000);
-  }
-
   update() {
-    // Update mesh position to match physics body
-    this.mesh.position.copy(this.body.position);
-    this.mesh.quaternion.copy(this.body.quaternion);
-
-    // Constant forward movement
-    this.body.velocity.z = -this.speed;
-  }
-
-  get position() {
-    return this.body.position;
+    // Move forward continuously
+    this.mesh.position.z -= this.speed;
   }
 }
